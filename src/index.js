07163e9ebeab0c76b6e928c83003e32d58fe66f7
J-Kuopus <kuopus79@gmail.com>
2022-09-07 12:04:49 +0100

    added basename to router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Project4 from './components/Project4/Project4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename='/'>
     <Routes>
       <Route path='/' element={<App />}/>
       <Route path='/impressum' element={<Impressum />}/>
